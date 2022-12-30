8c7b85bb589fcb3e099fb70c30fab2454eed0863
Ignorant <ignorantgrind@gmx.net>
2022-12-30 06:14:43 +0100

    routing repaired

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import "react-app-polyfill/ie9";
 import "react-app-polyfill/stable";
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { Router } from "wouter";
 import Layout from "./components/Layout/Layout";
 //import TestApp from "./components/TestApp/TestApp";
 import runConfig from "./config";
@@ -12,6 +13,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <Layout />
+    <Router base="/krachpage">
+      <Layout />
+    </Router>
   </React.StrictMode>
 );
